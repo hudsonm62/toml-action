46518159c4beeb95234540048666f62a2b56a410
Hudson <hudson@ziroau.com>
2026-03-14 15:18:52 +1100

    Fix field lookup and file read encoding
    - Read the file with utf8 and use const/let for declarations.
    - Replace forEach with a for...of loop so we can break early and handle missing fields: if a field is missing we now throw only when fail-missing-field is true, otherwise set value to undefined and stop.
    - This prevents an error when accessing nested properties on undefined.

--- index.js
+++ index.js
@@ -7,15 +7,19 @@ try {
     const field = core.getInput("field");
     const failMissingField = core.getBooleanInput("fail-missing-field");
     var fields = field.split(".");
-    var str = fs.readFileSync(file);
+    var str = fs.readFileSync(file, "utf8");
     var parsed = toml.parse(str);
     var value = parsed;
-    fields.forEach(function (f) {
-        if (value?.[f] === undefined && failMissingField) {
-            throw new Error(`Field '${field}' is missing in '${file}'`);
+    for (const f of fields) {
+        if (value?.[f] === undefined) {
+            if (failMissingField) {
+                throw new Error(`Field '${field}' is missing in '${file}'`);
+            }
+            value = undefined;
+            break;
         }
         value = value[f];
-    });
+    }
     core.setOutput("value", value);
 } catch (error) {
     core.setFailed(error.message);
