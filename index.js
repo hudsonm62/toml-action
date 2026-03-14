909b337320cb1f4fcaf32f8c0e83ee06cd2dee77
Hudson <hudson@ziroau.com>
2026-03-14 14:50:56 +1100

    Add `fail-missing-field` input

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const fs = require("fs");
 try {
     const file = core.getInput("file");
     const field = core.getInput("field");
+    const failMissingField = core.getBooleanInput("fail-missing-field");
     var fields = field.split(".");
     var str = fs.readFileSync(file);
     var parsed = toml.parse(str);
     var value = parsed;
     fields.forEach(function (f) {
+        if (value?.[f] === undefined && failMissingField) {
+            throw new Error(`Field '${field}' is missing in '${file}'`);
+        }
         value = value[f];
     });
     core.setOutput("value", value);
